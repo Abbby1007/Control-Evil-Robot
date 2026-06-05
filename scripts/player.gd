ddecdde8ec26fc7d1147ceb0e15a6cc92c22d81c
Abbby1007 <Abigailloken25@gmail.com>
2026-06-05 17:26:17 +0900

    Set up the basic level and player movement

---
+++ scripts/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+#8 way movement for the player
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left","right","up","down")
+	velocity = input_direction * speed
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_input()
+	move_and_slide()
