49d43d8906e6147efe03167256a31bacb9a2b721
Abbby1007 <Abigailloken25@gmail.com>
2026-06-05 21:51:37 +0900

    Main the layout of the tutorial level

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var robot: CharacterBody2D = $Robot
-var robotSpeed = 100
+var robotSpeed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
